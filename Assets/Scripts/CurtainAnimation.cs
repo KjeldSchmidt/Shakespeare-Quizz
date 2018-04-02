124d488c28661a68ef47b171497b84e743f0a7ba
Kjeld Schmidt <superfluidmercury@live.de>
2018-04-02 17:50:12 +0200

    Curtains animation now happens only once!

---
+++ Assets/Scripts/CurtainAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurtainAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Animator>()["Vorhang 0"].wrapMode = WrapMode.Once;
+		GetComponent<Animator>().Play("Vorhang 0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

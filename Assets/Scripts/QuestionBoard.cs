6e238940401c6f35a483aa0620035d1b2c280587
Kjeld Schmidt <superfluidmercury@live.de>
2018-04-05 14:01:35 +0200

    Added the question board an move up down animations

---
+++ Assets/Scripts/QuestionBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBoard : MonoBehaviour {
+	private bool isDown = true;
+	private Animator anim;
+	private int downHash = Animator.StringToHash("MoveDown");
+	private int upHash = Animator.StringToHash("MoveUp");
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	public void moveUp() {
+		if (isDown) {
+			anim.SetTrigger(upHash);
+			isDown = !isDown;
+		}
+	}
+
+	public void moveDown() {
+		if (!isDown) {
+			anim.SetTrigger(downHash);
+			isDown = !isDown;
+		}
+	}
+
+	void OnMouseDown() {
+		moveUp();
+	}
+}
